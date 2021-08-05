6d826f6bb50bde7835d60597ed6f39875f1fb260
Elyas <elyassalar8@gmail.com>
2021-08-05 20:59:28 +0300

    twelveth assignment solution

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -12,11 +12,11 @@ function ColorList() {
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <li style={{ color: colors[0] }}>{colors[0]}</li>
-      <li style={{ color: colors[1] }}>{colors[1]}</li>
-      <li style={{ color: colors[2] }}>{colors[2]}</li>
-      <li style={{ color: colors[3] }}>{colors[3]}</li>
-      <li style={{ color: colors[4] }}>{colors[4]}</li>
+      {
+        colors.map(eachColor => {
+          return <li key={eachColor} style={{ color: eachColor }}>{eachColor}</li>;
+        })
+      }
     </div>
   );
 }
